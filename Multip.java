0030942aa3350adf404102356be9bdacfa57449b
rocky-rev <136152822+rocky-rev@users.noreply.github.com>
2023-06-10 12:27:27 +0530

    Add files via upload

---
+++ Multip.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+class Multip{
+
+public static void main(String[] args) {
+
+    int a=10;
+    int b=20;
+    System.out.println("The multiplication is :"+(a*b));
+    
+} 
+    
+}
